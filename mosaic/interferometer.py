ea0bad5bd7d03b99110a83b438f60b8efb043588
Emma Carli <39613453+emmacarli@users.noreply.github.com>
2021-04-08 17:03:02 +0100

    Print TAB angle

--- mosaic/interferometer.py
+++ mosaic/interferometer.py
@@ -418,11 +418,10 @@ class InterferometryObservation:
 
         self.beamSize = [width1.degree, width2.degree]
 
-        # logger.info("axis1: {:.3g}, axis2: {:.3g}, angle: {:.3f} in pixel plane"
-                # .format(axis1, axis2, angle))
-
-        logger.info("beamshape: width1: {:.3g} arcsec, width2: {:.3g} arcsec in equatorial plane"
+        logger.info("beamshape: width1: {:.5g} arcsec, width2: {:.5g} arcsec, angle: {:.3g} wrt equatorial plane"
                 .format(width1.arcsecond, width2.arcsecond, angle))
+        #logger.info("beamshape: width1: {:.3g} arcsec, width2: {:.3g} arcsec in equatorial plane"
+                #.format(width1.arcsecond, width2.arcsecond, angle))
 
     def createContour(self, antennas, fileName=None, minAlt=0):
 
